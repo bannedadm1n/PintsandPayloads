5442c62a9793607583a97c6c5d721fda9bb1a4ee
bannedadm1n <bannedadm1n@protonmail.com>
2026-08-20 13:01:06 +0100

    bits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://pintsandpayloads.com'
+  site: 'https://pintsandpayloads.com',
+  adapter: cloudflare()
 });
